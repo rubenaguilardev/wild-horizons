66716071e7919dcd33b68cb71e588eb120148039
Ruben Aguilar <ragui56@wgu.edu>
2026-04-03 01:22:53 -0700

    create server, add data, db

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from the server')
+const server = http.createServer(async (req, res) => {
+
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.end(JSON.stringify(destinations))
+  }
+  
 })
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
