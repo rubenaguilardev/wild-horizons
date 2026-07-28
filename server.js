cc0083c2596d9f69c4c88540a8e49c93424d2e60
Ruben Aguilar <ragui56@wgu.edu>
2026-07-28 16:57:12 -0700

    add data, serve json

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import http from 'node:http'
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-  res.end('This is from the server')
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.end('This is from the server')
+  }
+  
 })
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
