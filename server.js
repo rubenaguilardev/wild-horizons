374c130c332d2cd59447382a2463420db429a4f3
Ruben Aguilar <ragui56@wgu.edu>
2026-07-28 17:41:18 -0700

    route not found

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(destinations))
+  } else {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
   }
   
 })
