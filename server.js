4f994fe6d321492563d7795b88ba150af36c5bb4
Ruben Aguilar <ragui56@wgu.edu>
2026-07-28 22:55:46 -0700

    add param

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(destinations))
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+    const continent = req.url.split('/').pop()
+    const filteredData = destinations.filter(destination => destination.continent.toLowerCase() === continent.toLowerCase())
+    console.log(filteredData)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(filteredData))
   } else {
     res.statusCode = 404
     res.setHeader('Content-Type', 'application/json')
