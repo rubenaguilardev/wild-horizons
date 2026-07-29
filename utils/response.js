5a3410ece78d0aa67a71497b590cd91941bae5ec
Ruben Aguilar <ragui56@wgu.edu>
2026-07-29 05:15:47 -0700

    add cors

--- utils/response.js
+++ utils/response.js
@@ -1,5 +1,7 @@
 const responseUtil = ((res, statusCode, payload) => {
   res.statusCode = statusCode
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify(payload))
 })
